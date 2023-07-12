fee31b7a2e243924f8473513342dad6f120adee8
Constantine <kosta.tovstik@gmail.com>
2023-07-12 19:53:18 +0300

    Deleted Layout component Updated .gitignore

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,29 +4,28 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
 import Test from "./pages/Test";
-import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Shoe from "./pages/Shoe";
 import Registration from "./pages/Registration";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={
-          <Layout children={
-            <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/test" element={<Test/>}/>
-              <Route path="*" element={<Error404/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/shoe/:id" element={<Shoe/>}/>
-              <Route path="/register" element={<Registration/>}/>
-            </Routes>
-          }/>
-        }/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navigation/>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/test" element={<Test/>}/>
+            <Route path="*" element={<Error404/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/shoe/:id" element={<Shoe/>}/>
+            <Route path="/register" element={<Registration/>}/>
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </>
   );
 }
 
